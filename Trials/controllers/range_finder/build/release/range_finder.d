7f499627ef7db72943f4472a23f724fcc8048840
Akshay V Nayak <vakshaynayak@gmail.com>
2021-06-24 01:53:11 +0530

    Initial trial

---
+++ Trials/controllers/range_finder/build/release/range_finder.d
@@ -0,0 +1,7 @@
+build/release/range_finder.o: range_finder.c \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/range_finder.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/utils/system.h
